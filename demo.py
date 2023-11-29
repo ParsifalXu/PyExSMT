6ff2edc7f5c6092c1bc701f08b50343070ab4aa3
ParsifalXu <xuxiufeng@pku.edu.cn>
2023-11-29 18:00:08 +0800

    update

--- demo.py
+++ demo.py
@@ -1,30 +1,82 @@
-# def demo(solver, penalty, dual):
-#     if solver == 'liblinear' and penalty == 'none':
-#         return False
 
-#     return solver
+def _fit(X, n_clusters, affinity, metric, memory, connectivity, compute_full_tree, linkage, distance_threshold, compute_distances, func_check_memory, func_callable, func_connectivity, func_check_array, func_len, func_max, func_memory_cache, func_func_memory_cache, func_np_count_nonzero, func__hc_cut, func__hierarchical_hc_get_heads, func_np_copy, func_np_unique, func_np_searchsorted):
+    memory = func_check_memory
 
+    _metric = metric
+    
+    if affinity != 'deprecated':
+        if metric != 'None':
+            return 'ERROR_END'
+        return 'WARNING_END'
+        _metric = affinity
+    elif metric == 'None':
+        _metric = 'euclidean'
 
+    if ((n_clusters != 'None' and distance_threshold == 'None') or (n_clusters == 'None' and distance_threshold != 'None')) != 0:
+        return 'ERROR_END'
 
-# # def demo(solver, penalty, dual):
-#     # all_solvers = ['liblinear', 'newton-cg', 'lbfgs', 'sag', 'saga']
-#     # if solver not in all_solvers:
-#     #     return False
+    if distance_threshold != 'None' and compute_full_tree != 0:
+        return 'ERROR_END'
 
-#     # all_penalties = ['l1', 'l2', 'elasticnet', 'none']
-#     # if penalty not in all_penalties:
-#     #     return False
+    if linkage == 'ward' and _metric != 'euclidean':
+        return 'ERROR_END'
 
-#     # if solver not in ['liblinear', 'saga'] and penalty not in ('l2', 'none'):
-#     #     return False
-#     # if solver != 'liblinear' and dual:
-#     #     return False
+    tree_builder = _TREE_BUILDERS[linkage]
 
-#     # if penalty == 'elasticnet' and solver != 'saga':
-#     #     return False
+    connectivity = connectivity
+    if connectivity != 'None':
+        if func_callable:
+            connectivity = func_connectivity
+        connectivity = func_check_array
 
-#     # if solver == 'liblinear' and penalty == 'none':
-#     #     return False
+    n_samples = func_len
+    compute_full_tree = compute_full_tree
+    if connectivity == 'None':
+        compute_full_tree = 'True'
+    if compute_full_tree == 'auto':
+        if distance_threshold != 'None':
+            compute_full_tree = 'True'
+        else:
+            
+            
+            
+            compute_full_tree = n_clusters < func_max
+    n_clusters = n_clusters
+    if compute_full_tree:
+        n_clusters = 'None'
 
-#     # return solver
+    
+    kwargs = {}
+    if linkage != 'ward':
+        kwargs['linkage'] = linkage
+        kwargs['affinity'] = _metric
 
+    distance_threshold = distance_threshold
+
+    return_distance = (distance_threshold != 'None') or compute_distances
+
+    out = func_func_memory_cache
+    (children_, n_connected_components_, n_leaves_, parents) = out[
+        :4
+    ]
+
+    if return_distance:
+        distances_ = out[-1]
+
+    if distance_threshold != 'None':  
+        n_clusters_ = (
+            func_np_count_nonzero + 1
+        )
+    else:  
+        n_clusters_ = n_clusters
+
+    
+    if compute_full_tree:
+        labels_ = func__hc_cut
+    else:
+        labels = func__hierarchical_hc_get_heads
+        
+        labels = func_np_copy
+        
+        labels_ = func_np_searchsorted
+    return 'END'
--- fit.py
+++ fit.py
@@ -1,19 +1,21 @@
-def fit(n_clusters, distance_threshold, compute_full_tree, X):
+def fit(compute_full_tree, n_clusters, distance_threshold, X, func_max, linkage, metric, func_abc_xyz_h):
     if not ((n_clusters != 'None' and distance_threshold == 'None') or (n_clusters == 'None' and distance_threshold != 'None')):
-        return False
+        return "END"
     
     if distance_threshold != 'None' and compute_full_tree == 'False':
-        return False
+        return "END"
+
+    if linkage == 'ward' and metric != 'euclidean':
+        return 'ERROR_END'
 
     if compute_full_tree == 'auto':
         if distance_threshold != 'None':
             compute_full_tree = 'True'
         else:
-            if n_clusters < max(100, 0.02 * X):
+            if n_clusters < func_max:
                 compute_full_tree = 'True'
             else:
                 compute_full_tree = 'False'
     if compute_full_tree:
         n_clusters = 'None'
-
-
+    return "END"
