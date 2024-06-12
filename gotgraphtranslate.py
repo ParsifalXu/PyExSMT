74aef06d2469c9e73a746ada8c7a9fa30c9f0e3f
ParsifalXu <43930877+ParsifalXu@users.noreply.github.com>
2024-06-12 13:33:14 +0800

    just use graph

--- gotgraphtranslate.py
+++ gotgraphtranslate.py
@@ -1,8 +1,28 @@
 import pygraphviz as pgv
 
 # Load the DOT file
-dot_file_path = 'xxx.dot'
-graph = pgv.AGraph(dot_file_path)
+dot_file_path = 'demo.py.dot'
+test = """digraph {
+"(in1 = 0)0" [ label="(in1 = 0)" ];
+"(in1 = 0)0" -> "(in2 = 0)1" [ label="1" ];
+"(in2 = 0)1" [ label="(in2 = 0)" ];
+"(in2 = 0)1" -> "02" [ label="1" ];
+"02" [ label="0" ];
+"(in2 = 0)1" -> "(in2 + in3)3" [ label="0" ];
+"(in2 + in3)3" [ label="(in2 + in3)" ];
+"(in1 = 0)0" -> "(0 < (in1 + in2))4" [ label="0" ];
+"(0 < (in1 + in2))4" [ label="(0 < (in1 + in2))" ];
+"(0 < (in1 + in2))4" -> "05" [ label="1" ];
+"05" [ label="0" ];
+"(0 < (in1 + in2))4" -> "(in2 = 0)6" [ label="0" ];
+"(in2 = 0)6" [ label="(in2 = 0)" ];
+"(in2 = 0)6" -> "07" [ label="1" ];
+"07" [ label="0" ];
+"(in2 = 0)6" -> "(in2 + in3)8" [ label="0" ];
+"(in2 + in3)8" [ label="(in2 + in3)" ];
+}
+"""
+graph = pgv.AGraph(test)
 
 # Function to extract paths
 def extract_paths(graph, node, path, paths):
